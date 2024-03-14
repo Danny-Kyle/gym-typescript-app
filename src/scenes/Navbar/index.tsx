a624b003ac61ce2e82867427ce84d27422b84f11
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-14 13:21:46 +0100

    ref fex regex

--- src/scenes/Navbar/index.tsx
+++ src/scenes/Navbar/index.tsx
@@ -1,54 +1,110 @@
-import {useState} from 'react'
-import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
-import Logo from "@/assets/Logo.png"
-import Link from './Link'
-import { SelectedPage } from '@/shared/types'
-import useMediaQuery from '@/hooks/useMediaQuery'
-import ActionButton from '@/shared/ActionButton'
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
-    selectedPage: SelectedPage;
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Navbar = ({selectedPage, setSelectedPage}: Props) => {
-    const flexBetween = "flex items-center justify-between";
-    const [toggleMenu, setToggleMenu] = useState<boolean>(false);
-    const isAboveMedScreen = useMediaQuery("(min-width: 1060px)");
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
+  const isAboveMedScreen = useMediaQuery("(min-width: 1060px)");
 
-    function handleToggle(){
-        setToggleMenu(!toggleMenu);
-    }
+  function handleToggle() {
+    setToggleMenu(!toggleMenu);
+  }
 
   return (
     <nav>
-        <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-            <div className={`${flexBetween} mx-auto w-5/6`}>
-                <div className={`${flexBetween} w-full gap-16`}>
-                    <img alt="logo" src={Logo} /> 
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img alt="logo" src={Logo} />
 
-
-                   {isAboveMedScreen ? (<div className={`${flexBetween} w-full`}>
-                        <div className={`${flexBetween} gap-8 text-sm`}>
-                            <Link page='Home' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                            <Link page='Benefits' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                            <Link page='Our Classes' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                            <Link page='Contact Us' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        </div>
-                        <div className={`"${flexBetween} gap-8`}>
-                            <p>Sign In</p>
-                            <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
-                        </div>
-                    </div>) : (
-                        <button className='rounded-full bg-secondary-500 p-2' onClick={handleToggle}>
-                            <Bars3Icon className='h-6 w-6 text-white' />
-                        </button>
-                    ) }
+            {isAboveMedScreen ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
                 </div>
-            </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={handleToggle}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
+          </div>
         </div>
-    </nav>
-  )
-}
+      </div>
 
-export default Navbar
+      {/*mobile */}
+      {!isAboveMedScreen && toggleMenu && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={handleToggle}>
+              <XMarkIcon className="h-6 w-6 text-gray-500" />
+            </button>
+          </div>
+
+          <div className={`ml-[33%] flex flex-col gap-10 text-2xl`}>
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
