10f9d107b5940b0592cd71f017bdc7a11db4dfc4
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-18 17:23:04 +0100

    benfits done

--- src/scenes/Benefits/index.tsx
+++ src/scenes/Benefits/index.tsx
@@ -43,16 +43,25 @@ const Benefits = ({setSelectedPage}: Props) => {
       <motion.div
       onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div className="md:my-5 md:w-3/5 ">
+        <motion.div 
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1.5 }}
+          variants={{ 
+              hidden: { opacity: 0, x: -50 }, 
+              visible: {opacity: 1, x: 0}
+          }}
+        className="md:my-5 md:w-3/5 ">
           <HText>MORE THEN JUST A GYM</HText>
           <p className="my-5 text-sm">
             We provide world class fitness equipment, trainers and classes to 
             get you to your ultimate fitness goals with ease. We provide true 
             care into each and every member
           </p>
-        </div>
+        </motion.div>
 
-        {/**indi benesfits */}
+        {/**indiv benefits */}
         <motion.div
         initial= "hidden"
         whileInView="visible"
