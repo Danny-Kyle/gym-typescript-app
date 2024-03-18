eb7bfd72958662a7a9240a7eed142cbc3f358d41
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-18 15:41:32 +0100

    style icon

--- src/scenes/Benefits/Benefit.tsx
+++ src/scenes/Benefits/Benefit.tsx
@@ -11,7 +11,13 @@ type Props = {
 
 const Benefit = ({icon, description, title, setSelectedPage}: Props) => {
   return (
-    <div>Benefit</div>
+    <div className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
+        <div className='mb-4 flex justify-center '>
+            <div className='rounded-full border-2 border-gray-100 bg-primary-100 p-4'>
+                {icon}
+            </div>
+        </div>
+    </div>
   )
 }
 
