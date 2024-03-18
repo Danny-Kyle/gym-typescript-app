64259a4ec9649c1327dbbe1fcda6f49ea107604e
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-18 18:35:20 +0100

    deploy now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/scenes/Navbar"
 import Home from "@/scenes/Home"
 import Benefits from "@/scenes/Benefits"
+// import OurClasses from "@/scenes/OurClasses"
 import { useEffect, useState } from "react"
 import { SelectedPage } from "@/shared/types"
 
@@ -24,7 +25,8 @@ function App() {
     <div className="app bg-gray-50">
      <Navbar topOfPage={topOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
      <Home setSelectedPage={setSelectedPage}/>
-     {/* <Benefits setSelectedPage={setSelectedPage}/> */}
+     <Benefits setSelectedPage={setSelectedPage}/>
+     {/* <OurClasses setSelectedPage={setSelectedPage}/> */}
     </div>
   )
 }
