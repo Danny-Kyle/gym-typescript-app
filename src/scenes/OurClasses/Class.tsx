1c19a44b44979d8f1c8bb196701ff97edb405f98
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-20 13:04:50 +0100

    our classes section

--- src/scenes/OurClasses/Class.tsx
+++ src/scenes/OurClasses/Class.tsx
@@ -8,9 +8,18 @@ type Props = {
     item:  ClassType
 }
 
+const overlayStyle = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90`
+
+
 const Class = ({item, name, description, image}: Props) => {
   return (
-    <li className='relative mx-5 inline-block h-[380px] w-[450px]'>Class</li>
+    <li className='relative mx-5 inline-block h-[380px] w-[450px]'>
+        <div className={overlayStyle}>
+            <p className='text-2xl font-semibold'>{item.name}</p>
+            <p className='mt-5'>{item.description}</p>
+        </div>
+        <img src={item.image} alt={item.name}/>
+    </li>
   )
 }
 
